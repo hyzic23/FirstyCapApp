e09ef6fbbacedfef8e92ea048286f862642fd356
isaac <isaac.oghenevwapo@gmail.com>
2026-09-12 02:55:32 -0400

    Update LoginScreen and added validation on button click

--- src/app/components/LoginScreen.tsx
+++ src/app/components/LoginScreen.tsx
@@ -5,31 +5,50 @@ import { useState } from "react";
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = () => {
-    console.log("Email:", email);
-    console.log("Password:", password);
+    if (!email) {
+      setError("Email is required");
+      return;
+    }
+
+    if (!password) {
+      setError("Password is required");
+      return;
+    }
+
+    setError("");
+
+    console.log("Login successful");
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
+      <Text style={styles.label}>Email</Text>
 
       <TextInput
         style={styles.input}
-        placeholder="Password"
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <Text style={styles.label}>Password</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
+
+      {error && <Text style={styles.error}>{error}</Text>}
 
       <Button title="Login" onPress={handleLogin} />
     </View>
@@ -50,11 +69,21 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+
   input: {
     borderWidth: 1,
+    borderColor: "#ccc",
     borderRadius: 8,
     padding: 12,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  error: {
+    color: "red",
     marginBottom: 15,
-    fontSize: 18,
   },
 });
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import { StyleSheet, View } from "react-native";
-import Counter from "./components/Counter";
+import LoginScreen from "./components/LoginScreen";
 
 export default function HomeScreen() {
+  //const [name, setName] = useState("");
+
   return (
     <View style={styles.container}>
-      <Counter title="My Counter" />
-      <Counter title="Another Counter" />
-      {/* <LoginScreen /> */}
+      <LoginScreen />
       {/* <Text style={styles.title}>What's your name?</Text>
 
       <TextInput
@@ -21,149 +21,29 @@ export default function HomeScreen() {
   );
 }
 
-//Number Counter
-// export default function HomeScreen() {
-//   const [count, setCount] = useState(0);
-
-//   const handleAdd = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Count: {count}</Text>
-
-//       <Button title="Add" onPress={handleAdd} />
-//     </View>
-//   );
-// }
-
-// export default function HomeScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Brume's Store</Text>
-
-//       <Welcome name="Ese Desire" />
-
-//       <Product name="iPhone" price={999} />
-//       <Product name="MacBook" price={1999} />
-//       <Product name="Airpods" price={249} />
-//     </View>
-//   );
-// }
-
-// export default function HomeScreen() {
-//   const handlePress = () => {
-//     Alert.alert("Button Pressed!");
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>Hello World!</Text>
-
-//       <Text style={styles.subtitle}>Welcome to my first React Native app.</Text>
-
-//       <View style={styles.button}>
-//         <Button title="Click Me" onPress={handlePress} />
-//       </View>
-//     </View>
-//   );
-// }
-
-// export default function Index() {
-//   const colorScheme = useColorScheme();
-//   const themeTextStyle =
-//     colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
-//   const themeContainerStyle =
-//     colorScheme === "light" ? styles.lightContainer : styles.darkContainer;
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Text> Content is in safe area.</Text>
-//       <Text style={{ fontFamily: "Inter", fontWeight: "700" }}>Inter Bold</Text>
-//       <Text
-//         style={{ fontFamily: "Inter", fontWeight: "700", fontStyle: "italic" }}
-//       >
-//         Inter Bold Italic
-//       </Text>
-//       <Text style={{ fontFamily: "FiraSans-MediumItalic" }}>
-//         Fira Sans Medium Italic
-//       </Text>
-//       <Ionicons name="checkmark-circle" size={32} color="green" />
-//       <Image source={{ uri: "example" }} style={{ width: 100, height: 100 }} />
-//       {/* <Image source={require("@/assets/images/react-logo.png")} /> */}
-//     </SafeAreaView>
-//   );
-// }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     padding: 20,
+    justifyContent: "center",
   },
 
   title: {
-    fontSize: 25,
-    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 15,
   },
 
   input: {
     borderWidth: 1,
+    borderColor: "#ccc",
     borderRadius: 8,
     padding: 12,
     fontSize: 18,
   },
 
   result: {
-    fontSize: 22,
+    fontSize: 20,
     marginTop: 20,
   },
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     padding: 20,
-//   },
-
-//   title: {
-//     fontSize: 25,
-//     //fontWeight: "bold",
-//     marginBottom: 20,
-//   },
-
-//   subtitle: {
-//     fontSize: 18,
-//     textAlign: "center",
-//   },
-
-//   button: {
-//     width: 200,
-//   },
-// });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   ext: {
-//     fontSize: 20,
-//   },
-//   lightContainer: {
-//     backgroundColor: "#d0d0c0",
-//   },
-//   darkContainer: {
-//     backgroundColor: "#242c40",
-//   },
-//   lightThemeText: {
-//     color: "#242c40",
-//   },
-//   darkThemeText: {
-//     color: "#d0d0c0",
-//   },
-// });
