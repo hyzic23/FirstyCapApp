12c2225fbbb2c4c33c40dbc5fa33539f3ebfbf21
isaac <isaac.oghenevwapo@gmail.com>
2026-09-16 22:44:09 -0400

    Created Product objects and moch data for products

---
+++ src/mocks/product.js
@@ -0,0 +1,22 @@
+export const MOCK_PRODUCTS = [
+  {
+    id: 1,
+    title: "iPhone 14 Pro Max",
+    price: 1250.99,
+  },
+  {
+    id: 2,
+    title: "MacBook Pro",
+    price: 4999.99,
+  },
+  {
+    id: 3,
+    title: "Airpod Gen 5",
+    price: 599.99,
+  },
+  {
+    id: 4,
+    title: "iWatch",
+    price: 899.99,
+  },
+];
